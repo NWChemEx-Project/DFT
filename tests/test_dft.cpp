4b1386050decf7263a74469aacc37c4fbbeac569
ebylaska <bylaska@gmail.com>
2019-06-19 16:32:30 -0700

    update test

--- tests/test_dft.cpp
+++ tests/test_dft.cpp
@@ -1,8 +1,26 @@
+
+#include <ga.h>
+#include <mpi.h>
+#include <macdecls.h>
+#include <ga-mpi.h>
 #include "dft/dft.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    dftlib();
-    return 0;
+  MPI_Init(&argc,&argv);
+  GA_Initialize();
+  MA_init(MT_DBL, 8000000, 20000000);
 
+  int mpi_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+
+  dftlib();
+
+  GA_Terminate();
+  MPI_Finalize();
+
+  return 0;
 }
+
+
+
